92d2f0426fb62b29720e4473174beccdd2054220
qiudengjiao <choudengjiao001@ke.com>
2019-09-30 17:56:36 +0800

    <add> 创建lib_ui库

---
+++ lib_ui/src/main/java/com/common/lib/ui/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.common.lib.ui.base;
+
+/**
+ * 创建时间: 2019/09/30 17:52 <br>
+ * 作者: qiudengjiao <br>
+ * 描述:
+ */
+public class BaseActivity {
+}
---
+++ lib_ui/src/main/java/com/common/lib/ui/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.common.lib.ui.base;
+
+/**
+ * 创建时间: 2019/09/30 17:53 <br>
+ * 作者: qiudengjiao <br>
+ * 描述:
+ */
+public class BaseFragment {
+
+}
