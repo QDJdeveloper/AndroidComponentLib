a30a9a8204b5a82aba5313183230b41a85b014bc
qiudengjiao <choudengjiao001@ke.com>
2019-12-31 17:51:27 +0800

    [feat]:封装Glide

---
+++ app/src/main/java/co/review/androidcommonlib/activity/ImageLoaderActivity.java
@@ -0,0 +1,11 @@
+package co.review.androidcommonlib.activity;
+
+import com.common.lib.ui.base.BaseActivity;
+
+/**
+ * 创建时间: 2019/12/31 11:44 <br>
+ * 作者: qiudengjiao <br>
+ * 描述:
+ */
+public class ImageLoaderActivity extends BaseActivity {
+}
---
+++ lib_ui/src/main/java/com/common/lib/ui/base/imageloader/ImageLoader.java
@@ -0,0 +1,9 @@
+package com.common.lib.ui.base.imageloader;
+
+/**
+ * 创建时间: 2019/12/31 11:58 <br>
+ * 作者: qiudengjiao <br>
+ * 描述:
+ */
+public class ImageLoader {
+}
